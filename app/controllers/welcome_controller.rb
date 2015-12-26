175dd5cc4ea6453eab0a11fcc895a0ec5bf82a68
Bruno Casali <brunoocasali@gmail.com>
2015-12-26 18:48:17 -0200

    upgrading welcome controller with new endpoints

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -15,7 +15,8 @@ class WelcomeController < ApplicationController
       who?: [{ name: 'Bruno Casali', contact: '@brunoocasali',
                email: 'brunoocasali@gmail.com' }],
       start: { docs: 'no docs, for a while', why: 'So that Zueira NEVER ends.',
-               end_points: %W(#{request.original_url}api/v1/),
+               end_points: %W(#{request.original_url}api/v1/clubs
+                              #{request.original_url}api/v1/users),
                home_page: 'https://github.com/zueirafc/api' }
     }
   end
