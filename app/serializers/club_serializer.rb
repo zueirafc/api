6aac3127fc582e5b1a1a99add4718087ec0cab40
Bruno Casali <brunoocasali@gmail.com>
2015-12-26 19:33:45 -0200

    updating the serializer

--- app/serializers/club_serializer.rb
+++ app/serializers/club_serializer.rb
@@ -1,4 +1,8 @@
 class ClubSerializer < ActiveModel::Serializer
-  attributes :id, :official_website, :shield, :official_facebook_page,
+  attributes :id, :official_website, :shield_url, :official_facebook_page,
              :official_twitter_page, :short_name, :full_name, :initials
+
+  def shield_url
+    object.shield_url
+  end
 end
