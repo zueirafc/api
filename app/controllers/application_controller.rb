fdbd4ae721fcfecb6251be75d377df5782d55d03
Bruno Casali <brunoocasali@gmail.com>
2015-12-27 01:39:46 -0200

    updated the father controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,19 @@ class ApplicationController < ActionController::API
   include ActionController::Serialization
   include Acl9::ControllerExtensions
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protect_from_forgery with: :null_session
+
   respond_to :json
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:username, :club_id)
+    end
+    
+    devise_parameter_sanitizer.for(:account_update) do |u|
+       u.permit(:username, :club_id)
+    end
+  end
 end
