8d1b61a64136d7ebc38c31e10aaf5449c0c919d7
Bruno Casali <brunoocasali@gmail.com>
2015-12-27 01:14:02 -0200

    changed the cors initializer

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,22 +1,13 @@
-Rails.application.config.middleware.insert_before 0, "Rack::Cors" do
-  allow do
-    origins '*'
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+module Api
+  class Application < Rails::Application
+    config.middleware.use Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+          headers: :any,
+          expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+          methods: [:get, :post, :options, :delete, :put]
+      end
+    end
   end
 end
-
-# module YourApp
-#   class Application < Rails::Application
-#     config.middleware.use Rack::Cors do
-#       allow do
-#         origins '*'
-#         resource '*',
-#           :headers => :any,
-#           :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
-#           :methods => [:get, :post, :options, :delete, :put]
-#       end
-#     end
-#   end
-# end
