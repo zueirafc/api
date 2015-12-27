a737463b82689da612c88d277171a09fdfd2345e
Bruno Casali <brunoocasali@gmail.com>
2015-12-27 01:59:19 -0200

    fixed problems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,17 +6,15 @@ class ApplicationController < ActionController::API
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protect_from_forgery with: :null_session
-
   respond_to :json
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) do |u|
       u.permit(:username, :club_id)
     end
-    
+
     devise_parameter_sanitizer.for(:account_update) do |u|
-       u.permit(:username, :club_id)
+      u.permit(:username, :club_id)
     end
   end
 end
