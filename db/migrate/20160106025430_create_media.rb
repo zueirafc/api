1d44c79943f65af255f7a14a5c042930a03132b4
Bruno Casali <brunoocasali@gmail.com>
2016-01-06 01:00:11 -0200

    updated db migration

--- db/migrate/20160106025430_create_media.rb
+++ db/migrate/20160106025430_create_media.rb
@@ -1,12 +1,14 @@
 class CreateMedia < ActiveRecord::Migration
   def change
     create_table :media do |t|
-      t.references :micropost, index: true, foreign_key: true
       t.string :file
-      t.integer :kind
+      t.integer :kind, index: true, null: false
+
+      t.references :micropost, index: true
 
       t.timestamps null: false
     end
-    add_index :media, :kind
+
+    add_foreign_key :media, :microposts, column: :micropost_id
   end
 end
