6155bf0f6df9001c8ac89bc74a879bf7070a4421
Bruno Casali <brunoocasali@gmail.com>
2016-01-15 13:13:29 -0200

    só rolo e confusão, TENTANDO DE NOVO

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -23,15 +23,17 @@ end
 
 CarrierWave.configure do |config|
   config.aws_credentials = {
-    aws_access_key_id: ENV['S3_ACCESS_KEY'],
-    aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
+    access_key_id: ENV['S3_ACCESS_KEY'],
+    secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
     region: 'us-west-2'
-    # ,
-    # path_style: true
   }
 
   config.aws_bucket  = ENV['S3_BUCKET_NAME']
-  config.aws_attributes = { 'Cache-Control' => "max-age=#{365.days.to_i}" }
+  
+  config.aws_attributes = {
+    expires: 1.week.from_now.httpdate,
+    cache_control: 'max-age=604800'
+  }
 
   if Rails.env.test?
     config.storage NullStorage
