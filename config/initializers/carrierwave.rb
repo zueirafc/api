6579f8e287f035e9502d1edfc9efc9147f5bc87f
Bruno Casali <brunoocasali@gmail.com>
2016-01-15 11:35:52 -0200

    updating s3 configs

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -25,7 +25,9 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['S3_ACCESS_KEY'],
-    aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY']
+    aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
+    region: 'us-west-2',
+    path_style: true
   }
   config.fog_directory  = ENV['S3_BUCKET_NAME']
   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.days.to_i}" }
