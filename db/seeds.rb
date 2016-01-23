d093c9e8b247044f4e0fdd3ddfaf1e1d1e8cc24c
Bruno Casali <brunoocasali@gmail.com>
2016-01-23 10:46:28 -0200

    updated clubs and each loop

---
+++ db/seeds.rb
@@ -0,0 +1,41 @@
+# Clubs:
+json_clubs = [
+{ official_website: "http://corinthians.com.br", shield: "", official_facebook_page: "https://facebook.com/corinthians", short_name: "Corinthians", full_name: "Sport Club Corinthians Paulista", initials: "SCCP" },
+{ official_website: "http://palmeiras.com.br", shield: "", official_facebook_page: "https://facebook.com/sePalmeiras", short_name: "Palmeiras", full_name: "Sociedade Esportiva Palmeiras", initials: "SEP" },
+{ official_website: "http://santosfc.com.br", shield: "", official_facebook_page: "https://facebook.com/santosfc", short_name: "Santos", full_name: "Santos Futebol Clube", initials: "SFC" },
+{ official_website: "http://saopaulofc.com.br", shield: "", official_facebook_page: "https://facebook.com/saopaulofc", short_name: "São Paulo", full_name: "São Paulo Futebol Clube", initials: "SPFC" },
+{ official_website: "http://gremio.com.br", shield: "", official_facebook_page: "https://facebook.com/MeuGremio", short_name: "Grêmio", full_name: "Grêmio Foot-Ball Porto Alegrense", initials: "GRE" },
+{ official_website: "http://atletico.com.br", shield: "", official_facebook_page: "https://facebook.com/clubeatleticomineiro", short_name: "Atlético-MG", full_name: "Clube Atlético Mineiro", initials: "CAM" },
+{ official_website: "http://atleticoparanaense.com", shield: "", official_facebook_page: "https://facebook.com/atleticopr", short_name: "Atlético-PR", full_name: "Clube Atlético Paranaense", initials: "CAP" },
+{ official_website: "http://cruzeiro.com.br", shield: "", official_facebook_page: "https://facebook.com/cruzeirooficial", short_name: "Cruzeiro", full_name: "Cruzeiro Esporte Clube", initials: "CRU" },
+{ official_website: "http://internacional.com.br", shield: "", official_facebook_page: "https://facebook.com/SCInternacional.Oficial", short_name: "Internacional", full_name: "Sport Club Internacional", initials: "INT" },
+{ official_website: "http://coritiba.com.br", shield: "", official_facebook_page: "https://facebook.com/coritibaoficial", short_name: "Coritiba", full_name: "Coritiba Foot Ball Club", initials: "CFC" },
+{ official_website: "http://sportrecife.com.br", shield: "", official_facebook_page: "https://facebook.com/sportclubdorecife", short_name: "Sport", full_name: "Sport Club do Recife", initials: "SPO" },
+{ official_website: "http://pontepreta.com.br", shield: "", official_facebook_page: "http://facebook.com/PontePretaOficial", short_name: "Ponte Preta", full_name: "Associação Atlética Ponte Preta", initials: "PON" },
+{ official_website: "http://flamengo.com.br", shield: "", official_facebook_page: "https://facebook.com/FlamengoOficial", short_name: "Flamengo", full_name: "Clube de Regatas do Flamengo", initials: "FLA" },
+{ official_website: "http://fluminense.com.br", shield: "", official_facebook_page: "https://facebook.com/FluminenseFC", short_name: "Fluminense", full_name: "Fluminense Football Club", initials: "FLU" },
+{ official_website: "http://chapecoense.com", shield: "", official_facebook_page: "https://facebook.com/AChapeF", short_name: "Chapecoense", full_name: "Associação Chapecoense de Futebol", initials: "CHA" },
+{ official_website: "http://figueirense.com.br", shield: "", official_facebook_page: "https://facebook.com/figueirensefc", short_name: "Figueirense", full_name: "Figueirense Futebol Clube", initials: "FIG" },
+{ official_website: "http://botafogo.com.br", shield: "", official_facebook_page: "http://facebook.com/BotafogoOficial", short_name: "Botafogo", full_name: "Botafogo de Futebol e Regatas", initials: "BOT" },
+{ official_website: "http://santacruzpe.com.br", shield: "", official_facebook_page: "https://facebook.com/OficialSantaCruzFC", short_name: "Santa Cruz", full_name: "Santa Cruz Futebol Clube", initials: "STA" },
+{ official_website: "http://ecvitoria.com.br", shield: "", official_facebook_page: "https://facebook.com/ecvitoriaoficial", short_name: "Vitória", full_name: "Esporte Clube Vitória", initials: "VIT" },
+{ official_website: "http://americamineiro.com.br", shield: "", official_facebook_page: "https://facebook.com/Americafcoficial", short_name: "América-MG", full_name: "América Futebol Clube", initials: "AMG" },
+{ official_website: "http://goiasec.com.br", shield: "", official_facebook_page: "https://facebook.com/goiasoficial", short_name: "Goiás", full_name: "Goiás Esporte Clube", initials: "GOI" },
+{ official_website: "http://avai.com.br", shield: "", official_facebook_page: "https://facebook.com/avaifc", short_name: "Avaí", full_name: "Avaí Futebol Clube", initials: "AVA" },
+{ official_website: "http://esporteclubebahia.com.br", shield: "", official_facebook_page: "https://facebook.com/ecbahia", short_name: "Bahia", full_name: "Esporte Clube Bahia", initials: "BAH" },
+{ official_website: "http://londrinaesporteclube.com.br", shield: "", official_facebook_page: "https://facebook.com/londrinaec", short_name: "Londrina", full_name: "Londrina Esporte Clube", initials: "LON" },
+{ official_website: "http://vasco.com.br", shield: "", official_facebook_page: "https://facebook.com/vascodagama", short_name: "Vasco da Gama", full_name: "Club de Regatas Vasco da Gama", initials: "VAS" },
+{ official_website: "http://nautico-pe.com.br", shield: "", official_facebook_page: "https://facebook.com/nauticope", short_name: "Náutico", full_name: "Clube Náutico Capibaribe", initials: "NAU" },
+{ official_website: "http://jec.com.br", shield: "", official_facebook_page: "https://facebook.com/JECoficial", short_name: "Joinville", full_name: "Joinville Esporte Clube", initials: "JEC" },
+{ official_website: "http://cearasc.com", shield: "", official_facebook_page: "https://facebook.com/CearaSC", short_name: "Ceará", full_name: "Ceará Sporting Club", initials: "CEA" }
+]
+
+# fazer loop aqui com os clubes. validando existentes pelo nome.
+json_clubs.each do |params|
+  next if Club.find_by_short_name(params[:short_name])
+
+  c = Club.new(params)
+  c.shield_url = params[:shield]
+  c.save!
+end
+# cadastrar sources
