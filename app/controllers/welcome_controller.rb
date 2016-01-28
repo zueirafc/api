195e3e54f179326c7710215f020e0928b5a9bb7c
Bruno Casali <brunoocasali@gmail.com>
2016-01-28 09:23:10 -0200

    added docs by json file

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,28 +7,7 @@ class WelcomeController < ApplicationController
 
   private
 
-  # rubocop:disable Metrics/MethodLength
   def make_a_hash
-    # Rails.application.routes.routes
-    @welcome =
-      {
-        name: 'ZueiraFCAPI',
-        for: 'Zueira Futebol Clube',
-        current_version: {
-          version: 'v1',
-          codename: 'Galatasaray',
-          status: 'development'
-        },
-        who?: [{
-          name: 'Bruno Casali',
-          contact: '@brunoocasali',
-          email: 'brunoocasali@gmail.com' }],
-        start: {
-          docs: 'no docs, for a while',
-          why: 'So that Zueira NEVER ends.',
-          end_points: 'TODO',
-          home_page: 'https://github.com/zueirafc/api'
-        }
-      }
+    @welcome = JSON.parse(File.read('docs/doc.json'))
   end
 end
