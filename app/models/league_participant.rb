4da7ee8d7222cec1b9ac2d25d80bac2b0fc6c715
Bruno Casali <brunoocasali@gmail.com>
2016-02-20 12:11:19 -0200

    fixed rubocop problems

--- app/models/league_participant.rb
+++ app/models/league_participant.rb
@@ -3,5 +3,5 @@ class LeagueParticipant < ActiveRecord::Base
   belongs_to :edition, class_name: 'LeagueEdition', foreign_key: :edition_id
 
   validates :club, presence: true
-  validates :edition, presence: true 
+  validates :edition, presence: true
 end
