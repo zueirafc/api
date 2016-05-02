e2d4af1794060a21e2d21046abf6bf9fd6cd1e64
Bruno Casali <brunoocasali@gmail.com>
2016-05-01 23:25:00 -0300

    created views layouts to emails and config vars inside test/production envs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,13 +26,12 @@ Rails.application.configure do
   config.action_mailer.default charset: 'utf-8'
 
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    domain: ENV['GMAIL_DOMAIN'],
-    authentication: 'plain',
-    enable_starttls_auto: true,
-    user_name: ENV['GMAIL_USERNAME'],
-    password: ENV['GMAIL_PASSWORD'],
-    openssl_verify_mode: 'none'
+    address:        'smtp.sendgrid.net',
+    port:           '587',
+    authentication: :plain,
+    user_name:      ENV['SENDGRID_USERNAME'],
+    password:       ENV['SENDGRID_PASSWORD'],
+    domain:         'heroku.com',
+    enable_starttls_auto: true
   }
 end
---
+++ spec/mailers/previews/contact_notifier_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/contact_notifier
+class ContactNotifierPreview < ActionMailer::Preview
+
+end
