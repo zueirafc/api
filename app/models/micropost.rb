50e05d8654d33df28c735aaf4c6c8b44aa29a288
Bruno Casali <brunoocasali@gmail.com>
2016-05-10 16:32:12 -0300

    created queries and tests

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -6,6 +6,11 @@ class Micropost < ActiveRecord::Base
   has_many :targets, dependent: :destroy
   has_many :trollers, dependent: :destroy
 
+  has_many :troller_clubs,
+           through: :trollers, source: :trollerable, source_type: 'Club'
+  has_many :target_clubs,
+           through: :targets, source: :targetable, source_type: 'Club'
+
   validates :provider_id, presence: true
   validates :shared, presence: true
   validates :text, presence: true
@@ -17,4 +22,23 @@ class Micropost < ActiveRecord::Base
                                create_scopes: true, create_helpers: true
 
   default_scope -> { order(created_at: :desc) }
+
+  ##
+  # Public: Get only posts based on nickname fan of a club as troller.
+  scope :troller_microposts_from_nick, lambda { |nick|
+    joins(:trollers).where(trollers: { trollerable: nick.club })
+  }
+
+  ##
+  # Public: Get only posts based on nickname fan of a club as target.
+  scope :target_microposts_from_nick, lambda { |nick|
+    joins(:targets).where(targets: { targetable: nick.club })
+  }
+
+  ##
+  # Public: Get posts based on nickname fans according of your type
+  #        (troller/target).
+  scope :versus_microposts_from_nicks, lambda { |troller, target|
+    troller_microposts_from_nick(troller).target_microposts_from_nick(target)
+  }
 end
