be37a97f7ec393a656b7c9c8d832ed3e3dcfef88
Bruno Casali <brunoocasali@gmail.com>
2016-05-15 22:20:40 -0300

    removed cors from api

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ gem 'rails-api'
 # infrastructure
 gem 'pg'
 gem 'passenger'
-gem 'rack-cors'
+gem 'rack-cors', require: 'rack/cors'
 
 # data manipulation
 gem 'enumerate_it'
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,13 +1,13 @@
-module Api
-  class Application < Rails::Application
-    config.middleware.use Rack::Cors do
-      allow do
-        origins '*'
-        resource '*',
-          headers: :any,
-          methods: [:get, :post, :delete, :put, :head],
-          expose: ['access-token', 'expiry', 'token-type', 'uid', 'client', 'Link']
-      end
-    end
-  end
-end
+# module Api
+#   class Application < Rails::Application
+#     config.middleware.use Rack::Cors do
+#       allow do
+#         origins '*'
+#         resource '*',
+#           headers: :any,
+#           methods: [:get, :post, :delete, :put, :head],
+#           expose: ['access-token', 'expiry', 'token-type', 'uid', 'client', 'Link']
+#       end
+#     end
+#   end
+# end
