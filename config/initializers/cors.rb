b357b35c8621890e45dbcd16458fa3fd3c83931d
Bruno Casali <brunoocasali@gmail.com>
2016-05-16 00:18:10 -0300

    :shipit: fixed auth things

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,13 +1,13 @@
-# module Api
-#   class Application < Rails::Application
-#     config.middleware.use Rack::Cors do
-#       allow do
-#         origins '*'
-#         resource '*',
-#           headers: :any,
-#           methods: [:get, :post, :delete, :put, :head],
-#           expose: ['access-token', 'expiry', 'token-type', 'uid', 'client', 'Link']
-#       end
-#     end
-#   end
-# end
+module Api
+  class Application < Rails::Application
+    config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+          :headers => :any,
+          :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+          :methods => [:get, :post, :options, :delete, :put]
+      end
+    end
+  end
+end
