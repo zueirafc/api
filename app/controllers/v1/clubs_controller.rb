0928bbf4882fc81fac8a147e902535a8ee01acea
Bruno Casali <brunoocasali@gmail.com>
2016-06-02 09:35:34 -0300

    Update clubs_controller.rb

--- app/controllers/v1/clubs_controller.rb
+++ app/controllers/v1/clubs_controller.rb
@@ -1,7 +1,8 @@
 module V1
   class ClubsController < ApplicationController
     before_action :set_club, only: [:show, :update, :destroy]
-
+    skip_before_action :authenticate_user!, only: :index
+    
     def index
       @clubs = Club.all
 
